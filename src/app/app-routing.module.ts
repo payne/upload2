4087cdfe2ab9498d13f5788e630cebb9ff74fe41
Matt Payne <Payne@MattPayne.org>
2023-02-26 11:19:29 -0600

    Connected upload-table to a route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {UploadTableComponent} from "./upload-table/upload-table.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'upload-table', component: UploadTableComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+
+}
--- src/app/upload-table/upload-table.component.ts
+++ src/app/upload-table/upload-table.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class UploadTableComponent {
 
+  uploads = [
+    {id: 1, name: 'upload1', type: 'dogs', date: '2020-01-01', status: 'published'},
+    {id: 2, name: 'upload2', type: 'solo', date: '2020-01-02', status: 'published'},
+    {id: 3, name: 'upload3', type: 'dogs', date: '2020-01-03', status: 'pending'},
+    {id: 4, name: 'upload4', type: 'Yahtzee', date: '2020-01-04', status: 'published'},
+    ];
+
+
+
 }
