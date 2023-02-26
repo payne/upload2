0d216e90f4553ea0e9796783852035b74d37ee23
Matt Payne <Payne@MattPayne.org>
2023-02-26 15:03:06 -0600

    Added some links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,10 @@ import {UploadTableComponent} from "./upload-table/upload-table.component";
 import {AboutComponent} from "./about/about.component";
 import {RegisterComponent} from "./register/register.component";
 import {LoginComponent} from "./login/login.component";
+import {CoursesComponent} from "./courses/courses.component";
 
 const routes: Routes = [
+  { path: '', component: CoursesComponent },
   { path: 'about', component: AboutComponent },
   { path: 'upload-table', component: UploadTableComponent },
   { path: 'register', component: RegisterComponent },
