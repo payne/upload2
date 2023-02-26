41b3afa8fcf08817ec54f84d89ca81ffdffaee6e
Matt Payne <Payne@MattPayne.org>
2023-02-26 11:08:40 -0600

    ng generate component upload_table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,12 @@ import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import {MatListModule} from "@angular/material/list";
+import { UploadTableComponent } from './upload-table/upload-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadTableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/upload-table/upload-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-table',
+  templateUrl: './upload-table.component.html',
+  styleUrls: ['./upload-table.component.scss']
+})
+export class UploadTableComponent {
+
+}
